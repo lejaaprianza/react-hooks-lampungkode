5a38955b41d93f7866510946d21715fcafa72a4f
lejaaprianza <lejaprianza@gmail.com>
2021-08-19 21:19:48 +0700

    edit file

--- src/ThemeButton.js
+++ src/ThemeButton.js
@@ -3,6 +3,7 @@ import { ThemeContext } from "./ThemeContex"
 
 function ThemedButton() {
   const theme = useContext(ThemeContext)
+  console.log(theme)
   return (
     <button style={{ background: theme.background, color: theme.foreground }}>
       I am styled by theme context!
